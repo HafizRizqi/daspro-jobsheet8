6912828eda95553974f066e094682ccf85e8d2f4
HafizRizqi <HafizRizqi>
2024-10-30 08:20:31 +0700

    Percobaan 4 sir

---
+++ RataNilai11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RataNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j;
+        float nilai,totalNilai, rataNilai = 0;
+
+        i = 1;
+        while (i <= 5) {
+            totalNilai = 0;
+            for(j=1; j <= 5; j++) {
+                System.out.print("Nilai ke-"+j + " = ");
+                int nilaiMhs = sc.nextInt();
+                totalNilai+=nilaiMhs;
+ 
+            }
+            rataNilai = totalNilai/5;
+            i++;
+            System.out.println("Rata- rata nilai Mahasiswa ke- "+i + " adalah " + rataNilai);
+        }
+    }
+}
